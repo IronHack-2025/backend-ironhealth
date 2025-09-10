31718e8522256244497b1d063ec3b9129eb6e83a
ccbaron <christian7baron@gmail.com>
2025-09-10 21:48:09 +0200

    Implemented Controllers and Routes OK

--- api/controllers/professionals.js
+++ api/controllers/professionals.js
@@ -56,6 +56,6 @@ export const getAllProfessionals = async(req, res) => {
         const professional = await Professional.find();
         res.status(200).json(professional)
     } catch (err){
-        res.status(500).json({ error: 'Error al obtener la lista de profesionales'})
+        res.status(500).json({ error: 'Error al obtener la lista de professionales'})
     }
 }
