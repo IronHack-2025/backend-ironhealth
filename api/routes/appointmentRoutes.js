68a51003219eec50a746714eac53ca528ff6a5f2
alenorgue <alenoriegag@gmail.com>
2025-09-14 10:38:40 +0200

    feat: add validateNotes middleware to sanitize and validate appointment notes

--- api/routes/appointmentRoutes.js
+++ api/routes/appointmentRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { postAppointments, getAppointments, deleteAppointments, cancelAppointments } from '../controllers/appointmentControllers.js';
+import { validateNotes } from '../middleware/validateNotes.js';
 
 const router = express.Router();
 
-router.post('/appointment', postAppointments);
+router.post('/appointment', validateNotes, postAppointments);
 router.get('/appointment', getAppointments);
 router.delete('/appointment/:id', deleteAppointments);
-router.put('/appointment/:id', cancelAppointments)
+router.put('/appointment/:id', cancelAppointments);
 
 export default router;
