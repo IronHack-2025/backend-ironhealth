4a695759e1dc877a17bdb3e3b9f4b82523a2c17f
ErebosXYZ <robertmaynou@gmail.com>
2025-09-17 21:11:46 +0200

    fix: ensure Cloudinary API secret is correctly utilized in signature generation

--- api/middlewares/getSignature.js
+++ api/middlewares/getSignature.js
@@ -4,8 +4,9 @@ export const getSignature = (req, res) => {
     const timestamp = Math.round(new Date().getTime() / 1000);
     const signature = cloudinary.utils.api_sign_request(
       { timestamp },
-      process.env.CLOUDINARY_API_SECRET
+       process.env.CLOUDINARY_API_SECRET
     );
+    console.log(timestamp, signature);
     res.json({
       timestamp,
       signature,
