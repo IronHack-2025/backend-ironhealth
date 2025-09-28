169f3484e09c0ccfdb6b9caa8d0bdef43491b72b
alenorgue <alenoriegag@gmail.com>
2025-09-28 17:19:06 +0200

    fix: change change-password route method from PUT to POST for consistency

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/login', login);
 
-router.put('/change-password', verifyToken, changePassword);
+router.post('/change-password', verifyToken, changePassword);
 
 router.post('/logout', verifyToken, logout);
 
